d7b0ad994c58c65ff2c70d0d8625e55c496c6f5c
KaczoREKT <s28876@pjwstk.edu.pl>
2025-03-28 01:32:31 +0100

    init

---
+++ InputEmbeddings.py
@@ -0,0 +1,16 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+class ImputEmbeddings(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+
+
+def forward(self, x) -> torch.Tensor:
+    return self.embedding(x) * math.sqrt(self.d_model)
